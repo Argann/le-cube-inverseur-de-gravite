bea7e12d3b24fbec8b04ba0c0093b321cebd74a2
Erwan AZZOUG <erwan.azzoug@etu.univ-nantes.fr>
2016-05-06 22:15:24 +0200

    Ajout script cube

---
+++ Assets/Scripts/QbeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QbeBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidbody2D;
+    [SerializeField]
+    private float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        this.rigidbody2D.AddForce(Vector2.up * this.jumpForce);
+	    }
+	}
+}
