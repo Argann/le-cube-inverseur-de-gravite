d88aa6c2de9f6bb9e2de5a74d71188e27811d81a
Argann <argann.b@hotmail.fr>
2016-05-06 23:04:39 +0200

    Ajout des scripts de génération de niveau, et de gestion des plateformes

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject platform;
+
+    [SerializeField]
+    private float platformSpeed;
+
+    [SerializeField]
+    private Vector2 startingPointUp;
+
+    [SerializeField]
+    private Vector2 startingPointDown;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float chanceUp;
+
+    public void Start()
+    {
+        InvokeRepeating("GeneratePlatform", 0f, 1f);
+    }
+
+    public void GeneratePlatform()
+    {
+        GameObject platform = Instantiate(this.platform);
+        if (Random.Range(0.0f, 1.0f) <= this.chanceUp)
+        {
+            platform.transform.position = this.startingPointUp;
+            platform.GetComponent<Rigidbody2D>().velocity = Vector2.left * platformSpeed;
+        } else
+        {
+            platform.transform.position = this.startingPointDown;
+            platform.GetComponent<Rigidbody2D>().velocity = Vector2.left * platformSpeed;
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlatformScript : MonoBehaviour {
+
+    public float secondsBeforeDestroy;
+
+    void Start()
+    {
+        Destroy(this.gameObject, this.secondsBeforeDestroy);
+    }
+
+    
+
+}
