4d0f891683c07e87b7e74601dd2ad807ce5352ff
Argann <argann.b@hotmail.fr>
2016-05-07 14:11:35 +0200

    Mise en place du score en cours

---
+++ Assets/Scripts/TimeScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScore : MonoBehaviour {
+
+    public int score;
+
+    public int scoreMultiplicator;
+
+	// Use this for initialization
+	void Start () {
+        this.score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.score += (int)(Time.deltaTime*this.scoreMultiplicator);
+	}
+}
