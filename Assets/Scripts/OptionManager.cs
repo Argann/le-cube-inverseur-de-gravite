9adde202caf818c1ded3433e9a30a68f25fd3e51
Erwan AZZOUG <erwan.azzoug@etu.univ-nantes.fr>
2016-05-08 08:30:26 +0200

    Pre-merge avec dev

---
+++ Assets/Scripts/OptionManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionManager : MonoBehaviour {
+
+    [SerializeField]
+    private Toggle musique;
+    [SerializeField]
+    private Toggle sons;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void OnValueChanged_Musique()
+    {
+        PlayerPrefs.SetInt("musique", musique.)
+    }
+}
